50611846a1f35010e5345f58f2933f005abc07cc
thoufiq135 <shaikno150@gmail.com>
2025-01-28 17:47:41 +0530

    Initial commit

---
+++ streaming/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './index.css'
+import src from "./src.jpg"
+
+function App() {
+  const [video, setvideo] = useState(false)
+  function play(){
+    if(video){
+      setvideo(false)
+    }else{
+      setvideo(true)
+    }
+  }
+
+  return (
+    <>
+    <h1>Songs</h1>
+    {video?
+    <>
+    <video src='http://localhost:7000/video' controls></video>
+    <button onClick={play}>Back</button>
+    </>:<>
+    <div id='par' onClick={play}>
+      <img src={src} onClick={play}></img>
+      <p onClick={play}>video is streaming through streams</p>
+    </div>
+    </>
+
+    }
+    </>
+  )
+}
+
+export default App
